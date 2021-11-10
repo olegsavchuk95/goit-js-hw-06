23f3262a6272feb6c1dc0dc5d25c187bc5b3f081
Oleg Savchuk <brandoleg@gmail.com>
2021-11-10 21:44:25 +0200

    finish

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,39 @@
+// Обработка отправки формы form.login-form должна быть по событию submit.
+// При отправке формы страница не должна перезагружаться.
+// Если в форме есть незаполненные поля, выводи alert с предупреждением о том, что все поля должны быть заполнены.
+// Если пользователь заполнил все поля и отправил форму, собери значения полей в обьект, где имя поля будет именем свойства, а значение поля - значением свойства. Для доступа к элементам формы используй свойство elements.
+// Выведи обьект с введенными данными в консоль и очисти значения полей формы методом reset.
 
+
+
+const form = document.querySelector('.login-form');
+const input = document.querySelector('.js-input')
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+
+    event.preventDefault();
+
+    const meaningEl = event.currentTarget.elements;
+
+    const email = meaningEl.email.value;
+    const password = meaningEl.password.value;
+
+    if(
+        form.elements.email.value === '' ||
+        form.elements.password.value === ''
+        ) {
+        return alert('Поля должны быть заполнены!')
+    }
+
+    const formData = {
+        email,
+        password,
+    };
+
+    console.log(formData);
+
+    form.reset();
+
+};
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,23 @@
+// Напиши скрипт, который изменяет цвета фона элемента <body> через инлайн стиль при клике на button.change-color и выводит значение цвета в span.color.
+
+// Для генерации случайного цвета используй функцию getRandomHexColor.
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const bodyColor = document.querySelector('body');
+const spanColor = document.querySelector('.color')
+const btnColor = document.querySelector('.change-color');
+
+
+btnColor.addEventListener('click', function () {
+  const color = getRandomHexColor(); 
+  
+  bodyColor.style.backgroundColor =  color;
+  spanColor.textContent = color;
+})
+
+
+
